e79d536cf29637ed87d008b59e04519de49e11b3
Gilbert Ngouo <149798152+Ngouo@users.noreply.github.com>
2026-08-29 13:54:56 +0100

    configuration des liens

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // Ajoutez chaque page ici :
+        home: resolve(__dirname, 'home.html'),
+        about: resolve(__dirname, 'about.html'),
+        boutique: resolve(__dirname, 'boutique.html'),
+      },
+    },
+  },
 });
